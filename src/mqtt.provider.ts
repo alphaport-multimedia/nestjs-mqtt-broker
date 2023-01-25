6befc6065b302cbcbdf11549fd523f47a9a3d0af
Sebastian Geschke <sebastian.geschke@gmail.com>
2023-01-25 13:52:04 +0100

    feat: use http to create server

--- src/mqtt.provider.ts
+++ src/mqtt.provider.ts
@@ -1,6 +1,6 @@
 import { Logger, Provider } from '@nestjs/common';
 import { Aedes } from 'aedes';
-import { createServer } from 'aedes-server-factory';
+import { createServer } from 'http';
 import { INSTANCE_BROKER, OPTION_PROVIDER } from './mqtt.constant';
 import { MqttModuleOptions } from './mqtt.interface';
 import { MqttModule } from './mqtt.module';
@@ -11,8 +11,15 @@ export function createClientProvider(): Provider {
     useFactory: async (options: MqttModuleOptions) => {
       // eslint-disable-next-line @typescript-eslint/no-var-requires
       const broker: Aedes = require('aedes')(options);
-      await createServer(broker, { ws: true }).listen(options.port);
-      Logger.log(`🚀 MQTT Broker is running on: ws://localhost:${options.port}`, MqttModule.name);
+      const server = await createServer();
+
+      const ws = require('websocket-stream');
+      ws.createServer({ server: server }, broker.handle);
+
+      server.listen(options.port, () => {
+        Logger.log(`🚀 MQTT Broker is running on: ws://localhost:${options.port}`, MqttModule.name);
+      });
+
       return broker;
     },
     inject: [OPTION_PROVIDER],
